56f3f2825b928c5f08a03fd18399c9970cfebecc
Larry Foulkrod <lefoulkrod@gmail.com>
2018-05-20 22:03:17 -0500

    modularized output component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import { fromEvent } from 'rxjs/observable/fromEvent';
 import { map } from 'rxjs/operators';
 import './style.css';
 import { logo } from './logo/logo';
+import { output } from './output/output.component';
 
 document.body.appendChild(logo);
-
-const output = document.createElement('span');
-output.classList.add('output');
-output.innerHTML = 'Hello world.  Click me to get started.';
 document.body.appendChild(output);
 
 fromEvent(document, 'click').pipe(
   map(event => `Last event time: ${event.timeStamp}`))
-  .subscribe(val => output.innerHTML = val);
+  .subscribe(val => output.setValue(val));
---
+++ src/output/output.component.js
@@ -0,0 +1,9 @@
+import './output.css';
+
+const span = document.createElement('span');
+span.classList.add('output');
+span.innerHTML = 'Hello world.  Click me to get started.';
+span.setValue = function(val) {
+  span.innerHTML = val;
+}
+export const output = span;
