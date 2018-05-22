ee69ff027d58c461fdd127bc05118e0ec5052195
Larry Foulkrod <lefoulkrod@gmail.com>
2018-05-22 10:35:34 -0500

    lazy loaded component

--- src/index.js
+++ src/index.js
@@ -5,17 +5,34 @@ import { logo } from './logo/logo';
 import { output } from './output/output.component';
 import { error } from './error/error.component';
 
+
+let origOutput = output;
+
 const subscribeToClick = (elem, outputComp) => {
   return fromEvent(elem, 'click').pipe(
     map(event => `Last event time: ${event.timeStamp}`))
     .subscribe(val => outputComp.setValue(val));
 };
-let origOutput = output;
 document.body.appendChild(logo);
-document.body.appendChild(origOutput);
+document.body.appendChild(output);
 document.body.appendChild(error);
 
-let subscription = subscribeToClick(document, origOutput);
+// create the button to lazy load.
+const frag = document.createDocumentFragment();
+const lazyContainer = document.createElement('div');
+lazyContainer.innerHTML = `<button>Click me to lazy load.</button>`;
+frag.appendChild(lazyContainer);
+document.body.appendChild(lazyContainer);
+
+lazyContainer.onclick = e => {
+  e.stopPropagation();
+  import(/* webpackChunkName: "LazyComponent" */ './lazy/lazy.component').then(module => {
+    let lazyComponent = new module.LazyComponent();
+    lazyComponent.mount(document.body);
+  });
+};
+
+let subscription = subscribeToClick(document, output);
 
 if (module.hot) {
   module.hot.accept('./output/output.component', function() {
---
+++ src/lazy/lazy.component.js
@@ -0,0 +1,17 @@
+export class LazyComponent {
+  constructor() {
+    this.elemRef = document.createElement('div');
+    this.elemRef.innerHTML = `I'm so lazy.`;
+  }
+
+  mount(parentElem) {
+    this.parentElemRef = parentElem;
+    parentElem.appendChild(this.elemRef);
+  }
+
+  unmount() {
+    this.parentElemRef.removeChild(this.elemRef);
+    this.elemRef = null;
+    this.parentElemRef = null;
+  }
+}
